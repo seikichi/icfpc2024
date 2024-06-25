6dfabedb65bd65a7db82f14e44499d6cb3b4a892
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-06-25 23:55:11 +0900

    Update sample to use sqlite

--- optimize/src/optimize/__init__.py
+++ optimize/src/optimize/__init__.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 import optuna
 
 def objective(trial):
@@ -5,7 +7,11 @@ def objective(trial):
     return (x - 2) ** 2
 
 def main() -> int:
-    study = optuna.create_study()
+    # https://optuna.readthedocs.io/en/stable/tutorial/20_recipes/001_rdb.html
+    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
+    study_name = "example-study"
+    storage_name = "sqlite:///{}.db".format(study_name)
+    study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True)
     study.optimize(objective, n_trials=100)
     print(study.best_params)  # E.g. {'x': 2.002108042}
     return 0

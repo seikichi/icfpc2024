8b95cf9f75350205cb8c6e895c33ef307ddc35f0
Yusuke Nojima <nojima@ynojima.com>
2024-06-29 03:47:58 +0900

    bug fix

--- solver/core/src/eval.rs
+++ solver/core/src/eval.rs
@@ -1,6 +1,6 @@
 use std::{
+    collections::HashMap,
     fmt::{Debug, Display},
-    num::ParseIntError,
     rc::Rc,
 };
 
@@ -269,8 +269,8 @@ pub fn parse(tokens: &[Token]) -> ParseResult<(Box<Node>, &[Token])> {
 
 #[derive(Debug, thiserror::Error)]
 pub enum EvalError {
-    #[error("type error: expected a value of type {0}, but the given value is {1}")]
-    TypeError(String, String),
+    #[error("{0}: type error: expected a value of type {1}, but the given value is {2}")]
+    TypeError(String, String, String),
 
     #[error("value error: {0}")]
     ValueError(String),
@@ -305,33 +305,78 @@ impl Frame {
 }
 
 macro_rules! type_check {
-    ($value:ident, $pattern:pat, $expected:expr) => {
+    ($value:ident, $pattern:pat, $expected:expr, $context:expr) => {
         let $pattern = $value else {
-            return Err(EvalError::TypeError($expected.into(), $value.to_string()));
+            return Err(EvalError::TypeError(
+                $context.into(),
+                $expected.into(),
+                $value.to_string(),
+            ));
         };
     };
+}
+
+static TABLE: &'static [u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
+
+static RTABLE: Lazy<HashMap<u8, usize>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+    for (i, c) in TABLE.iter().enumerate() {
+        m.insert(*c, i);
+    }
+    m
+});
+
+fn string_to_int_94(s: &str) -> EvalResult<i64> {
+    let mut ret = 0;
+    for c in s.bytes() {
+        let index = RTABLE
+            .get(&c)
+            .ok_or_else(|| EvalError::ValueError(format!("string-to-int: invalid number: {s}")))?;
+        ret = ret * 94 + index;
+    }
+    Ok(ret as i64)
+}
+
+fn int_to_string_94(mut i: i64) -> String {
+    if i == 0 {
+        return "!".into();
+    }
+    let mut neg = false;
+    if i < 0 {
+        neg = true;
+        i = -i;
+    }
+    let mut ret = Vec::new();
+    while i > 0 {
+        ret.push(TABLE[(i % 94) as usize]);
+        i /= 94;
+    }
+    if neg {
+        ret.push(b'-');
+    }
+    ret.reverse();
+    String::from_utf8_lossy(&ret).to_string()
 }
 
 fn eval_unary_op(frame: Rc<Frame>, op: UnaryOp, operand: &Node) -> EvalResult<Value> {
     let inner_value = eval(frame, operand)?;
     match op {
         UnaryOp::Neg => {
-            type_check!(inner_value, Value::Int(i), "Int");
+            type_check!(inner_value, Value::Int(i), "Int", "neg");
             Ok(Value::Int(-i))
         }
         UnaryOp::Not => {
-            type_check!(inner_value, Value::Bool(b), "Bool");
+            type_check!(inner_value, Value::Bool(b), "Bool", "not");
             Ok(Value::Bool(!b))
         }
         UnaryOp::IntToString => {
-            type_check!(inner_value, Value::Int(i), "Int");
-            Ok(Value::String(i.to_string()))
+            type_check!(inner_value, Value::Int(i), "Int", "int-to-string");
+            let s = int_to_string_94(i);
+            Ok(Value::String(s))
         }
         UnaryOp::StringToInt => {
-            type_check!(inner_value, Value::String(s), "String");
-            let i: i64 = s
-                .parse()
-                .map_err(|e: ParseIntError| EvalError::ValueError(e.to_string()))?;
+            type_check!(inner_value, Value::String(s), "String", "string-to-int");
+            let i = string_to_int_94(&s)?;
             Ok(Value::Int(i))
         }
     }
@@ -341,7 +386,12 @@ fn eval_apply(frame: Rc<Frame>, func: &Node, arg: &Node) -> EvalResult<Value> {
     // 本来は遅延評価しないといけない
     let f_value = eval(Rc::clone(&frame), func)?;
     let a_value = eval(frame, arg)?;
-    type_check!(f_value, Value::Closure(c_frame, c_var, c_body), "Closure");
+    type_check!(
+        f_value,
+        Value::Closure(c_frame, c_var, c_body),
+        "Closure",
+        "apply"
+    );
     let new_frame = Rc::new(c_frame.with_variable(c_var, Rc::new(a_value)));
     eval(new_frame, &c_body)
 }
@@ -355,66 +405,66 @@ fn eval_bin_op(frame: Rc<Frame>, op: BinOp, lhs: &Node, rhs: &Node) -> EvalResul
     let r_value = eval(frame, rhs)?;
     match op {
         BinOp::Add => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "add(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "add(rhs)");
             Ok(Value::Int(l + r))
         }
         BinOp::Sub => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "sub(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "sub(rhs)");
             Ok(Value::Int(l - r))
         }
         BinOp::Mul => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "mul(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "mul(rhs)");
             Ok(Value::Int(l * r))
         }
         BinOp::Div => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "div(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "div(rhs)");
             Ok(Value::Int(l / r)) // TODO: 丸め方向が正しいかチェックする
         }
         BinOp::Mod => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "mod(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "mod(rhs)");
             Ok(Value::Int(l % r)) // TODO: 負の数に対する挙動が正しいかチェックする
         }
         BinOp::Lt => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "lt(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "lt(rhs)");
             Ok(Value::Bool(l < r))
         }
         BinOp::Gt => {
-            type_check!(l_value, Value::Int(l), "Int");
-            type_check!(r_value, Value::Int(r), "Int");
+            type_check!(l_value, Value::Int(l), "Int", "gt(lhs)");
+            type_check!(r_value, Value::Int(r), "Int", "gt(rhs)");
             Ok(Value::Bool(l > r))
         }
         BinOp::Eq => Ok(Value::Bool(l_value == r_value)),
         BinOp::Or => {
-            type_check!(l_value, Value::Bool(l), "Bool");
-            type_check!(r_value, Value::Bool(r), "Bool");
+            type_check!(l_value, Value::Bool(l), "Bool", "or(lhs)");
+            type_check!(r_value, Value::Bool(r), "Bool", "or(rhs)");
             Ok(Value::Bool(l || r))
         }
         BinOp::And => {
-            type_check!(l_value, Value::Bool(l), "Bool");
-            type_check!(r_value, Value::Bool(r), "Bool");
+            type_check!(l_value, Value::Bool(l), "Bool", "and(lhs)");
+            type_check!(r_value, Value::Bool(r), "Bool", "and(rhs)");
             Ok(Value::Bool(l && r))
         }
         BinOp::Concat => {
-            type_check!(l_value, Value::String(mut l), "String");
-            type_check!(r_value, Value::String(r), "String");
+            type_check!(l_value, Value::String(mut l), "String", "concat(lhs)");
+            type_check!(r_value, Value::String(r), "String", "concat(rhs)");
             l.push_str(&r);
             Ok(Value::String(l))
         }
         BinOp::Take => {
-            type_check!(l_value, Value::String(s), "String");
-            type_check!(r_value, Value::Int(n), "Int");
+            type_check!(l_value, Value::Int(n), "Int", "take(lhs)");
+            type_check!(r_value, Value::String(s), "String", "take(rhs)");
             let ret = s.chars().take(n as usize).collect(); // TODO: パフォーマンスの問題があるかも
             Ok(Value::String(ret))
         }
         BinOp::Drop => {
-            type_check!(l_value, Value::String(s), "String");
-            type_check!(r_value, Value::Int(n), "Int");
+            type_check!(l_value, Value::Int(n), "Int", "drop(lhs)");
+            type_check!(r_value, Value::String(s), "String", "drop(rhs)");
             let ret = s.chars().skip(n as usize).collect(); // TODO: パフォーマンスの問題があるかも
             Ok(Value::String(ret))
         }
@@ -424,7 +474,7 @@ fn eval_bin_op(frame: Rc<Frame>, op: BinOp, lhs: &Node, rhs: &Node) -> EvalResul
 
 fn eval_if(frame: Rc<Frame>, cond: &Node, then: &Node, else_: &Node) -> EvalResult<Value> {
     let cond_value = eval(Rc::clone(&frame), cond)?;
-    type_check!(cond_value, Value::Bool(b), "Bool");
+    type_check!(cond_value, Value::Bool(b), "Bool", "if(cond)");
     if b {
         eval(frame, then)
     } else {

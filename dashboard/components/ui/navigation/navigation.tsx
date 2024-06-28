54a2d41a35b7842d2b209662b37e61bd228025c1
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-06-28 22:54:25 +0900

    Add jests

--- dashboard/components/ui/navigation/navigation.tsx
+++ dashboard/components/ui/navigation/navigation.tsx
@@ -3,12 +3,12 @@
 import { siteConfig } from "@/app/siteConfig";
 import {
   RiHome2Line,
-  RiTableLine,
-  RiBarChartLine,
-  RiCodeLine,
-  RiDatabaseLine,
-  RiLinkM,
-  RiInputField,
+  // RiTableLine,
+  // RiBarChartLine,
+  // RiCodeLine,
+  // RiDatabaseLine,
+  // RiLinkM,
+  // RiInputField,
 } from "@remixicon/react";
 
 export const navigation = [
@@ -19,17 +19,17 @@ export const navigation = [
   //     href: siteConfig.baseLinks.settings,
   //     icon: RiSettings5Line,
   //   },
-  { name: "Tables", href: siteConfig.baseLinks.tables, icon: RiTableLine },
-  { name: "Charts", href: siteConfig.baseLinks.charts, icon: RiBarChartLine },
-  { name: "WASM", href: siteConfig.baseLinks.wasm, icon: RiCodeLine },
-  { name: "Database", href: siteConfig.baseLinks.db, icon: RiDatabaseLine },
-  { name: "Forms", href: siteConfig.baseLinks.forms, icon: RiInputField },
+  // { name: "Tables", href: siteConfig.baseLinks.tables, icon: RiTableLine },
+  // { name: "Charts", href: siteConfig.baseLinks.charts, icon: RiBarChartLine },
+  // { name: "WASM", href: siteConfig.baseLinks.wasm, icon: RiCodeLine },
+  // { name: "Database", href: siteConfig.baseLinks.db, icon: RiDatabaseLine },
+  // { name: "Forms", href: siteConfig.baseLinks.forms, icon: RiInputField },
 ] as const;
 
 export const shortcuts = [
-  {
-    name: "Run WASM",
-    href: siteConfig.baseLinks.wasm,
-    icon: RiLinkM,
-  },
+  // {
+  //   name: "Run WASM",
+  //   href: siteConfig.baseLinks.wasm,
+  //   icon: RiLinkM,
+  // },
 ] as const;

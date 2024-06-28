76673db962f8b9e81ce5c4d3e654ae60285d33e1
Yusuke Nojima <nojima@ynojima.com>
2024-06-29 01:01:12 +0900

    [wip] implement parser

--- solver/core/src/eval.rs
+++ solver/core/src/eval.rs
@@ -1,10 +1,10 @@
-use std::{fmt::{write, Debug, Display}, sync::OnceLock};
+use std::fmt::{Debug, Display};
 
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 #[derive(Debug, PartialEq, Eq)]
-enum Value {
+pub enum Value {
     Bool(bool),
     Int(i64),
     String(String),
@@ -17,13 +17,13 @@ impl Display for Value {
             Value::Bool(b) => write!(f, "{b}"),
             Value::Int(i) => write!(f, "{i}"),
             Value::String(s) => write!(f, "{:?}", s),
-            Value::Closure(n_args) => write!(f, "<closure {n_args}>")
+            Value::Closure(v) => write!(f, "<closure v{v}>"),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-enum UnaryOp {
+pub enum UnaryOp {
     Neg,
     Not,
     StringToInt,
@@ -42,7 +42,7 @@ impl Display for UnaryOp {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-enum BinOp {
+pub enum BinOp {
     Add,
     Sub,
     Mul,
@@ -54,8 +54,8 @@ enum BinOp {
     Or,
     And,
     Concat,
-    Take, // Take first x chars of string y
-    Drop, // Drop first x chars of string y
+    Take,  // Take first x chars of string y
+    Drop,  // Drop first x chars of string y
     Apply, // Apply term x to y
 }
 
@@ -72,7 +72,7 @@ impl Display for BinOp {
             BinOp::Eq => write!(f, "=="),
             BinOp::Or => write!(f, "||"),
             BinOp::And => write!(f, "&&"),
-            BinOp::Concat => write!(f, "++"),
+            BinOp::Concat => write!(f, ".."),
             BinOp::Take => write!(f, "`take`"),
             BinOp::Drop => write!(f, "`drop`"),
             BinOp::Apply => write!(f, "$"),
@@ -81,8 +81,9 @@ impl Display for BinOp {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-enum Node {
+pub enum Node {
     Literal(Value),
+    Variable(usize),
     UnaryOp(UnaryOp, Box<Node>),
     BinOp(BinOp, Box<Node>, Box<Node>),
     If(Box<Node>, Box<Node>, Box<Node>),
@@ -96,7 +97,8 @@ impl Display for Node {
             Node::UnaryOp(op, node) => write!(f, "{op}{node}"),
             Node::BinOp(op, l, r) => write!(f, "({l} {op} {r})"),
             Node::If(cond, then, else_) => write!(f, "if {cond} then {then} else {else_}"),
-            Node::Lambda(n_args, body) => write!(f, "λ{n_args}. {body}"),
+            Node::Lambda(var, body) => write!(f, "λ v{var}: {body}"),
+            Node::Variable(var) => write!(f, "v{var}"),
         }
     }
 }
@@ -104,18 +106,24 @@ impl Display for Node {
 type Token = String;
 
 #[derive(Debug, thiserror::Error)]
-enum ParseError {
+pub enum ParseError {
     #[error("unexpected EOF")]
     UnexpectedEof,
 
     #[error("unknown indicator: {0}")]
     UnknownIndicator(char),
 
-    #[error("invalid integer literal")]
-    InvalidIntegerLiteral,
+    #[error("invalid integer literal: body={0}")]
+    InvalidIntegerLiteral(String),
 
-    #[error("invalid string literal")]
-    InvalidStringLiteral,
+    #[error("invalid string literal: body={0}")]
+    InvalidStringLiteral(String),
+
+    #[error("invalid unary op: body={0}")]
+    InvalidUnaryOp(String),
+
+    #[error("invalid binary op: body={0}")]
+    InvalidBinaryOp(String),
 }
 
 type ParseResult<T> = Result<T, ParseError>;
@@ -129,15 +137,20 @@ fn ok(node: Node, rest: &[Token]) -> ParseResult<(Box<Node>, &[Token])> {
     Ok((Box::new(node), rest))
 }
 
-fn parse_integer(body: &str) -> ParseResult<Box<Node>> {
+fn parse_integer_raw(body: &str) -> ParseResult<i64> {
     let mut ret: i64 = 0;
     for c in body.chars() {
         let i = c as i64;
         if i < 33 || i >= 33 + 94 {
-            return Err(ParseError::InvalidIntegerLiteral);
+            return Err(ParseError::InvalidIntegerLiteral(body.into()));
         }
         ret = ret * 94 + (i - 33);
     }
+    Ok(ret)
+}
+
+fn parse_integer(body: &str) -> ParseResult<Box<Node>> {
+    let ret = parse_integer_raw(body)?;
     Ok(Box::new(Node::Literal(Value::Int(ret))))
 }
 
@@ -153,7 +166,7 @@ fn parse_string(body: &str) -> ParseResult<Box<Node>> {
     for c in body.chars() {
         let i = c as usize;
         if i < 33 || i >= 33 + 94 {
-            return Err(ParseError::InvalidStringLiteral);
+            return Err(ParseError::InvalidStringLiteral(body.into()));
         }
         let decoded = char::from_u32(TABLE[i - 33] as u32).unwrap();
         ret.push(decoded);
@@ -165,6 +178,44 @@ fn parse_string(body: &str) -> ParseResult<Box<Node>> {
 fn test_parse_string() {
     let node = parse_string("B%,,/}Q/2,$_").unwrap();
     assert_eq!(*node, Node::Literal(Value::String("Hello World!".into())));
+}
+
+fn parse_unary_op(body: &str) -> ParseResult<UnaryOp> {
+    if body.len() != 1 {
+        return Err(ParseError::InvalidUnaryOp(body.into()));
+    }
+    let op = body.chars().next().unwrap();
+    match op {
+        '-' => Ok(UnaryOp::Neg),
+        '!' => Ok(UnaryOp::Not),
+        '#' => Ok(UnaryOp::StringToInt),
+        '$' => Ok(UnaryOp::IntToString),
+        _ => Err(ParseError::InvalidUnaryOp(body.into())),
+    }
+}
+
+fn parse_bin_op(body: &str) -> ParseResult<BinOp> {
+    if body.len() != 1 {
+        return Err(ParseError::InvalidUnaryOp(body.into()));
+    }
+    let op = body.chars().next().unwrap();
+    match op {
+        '+' => Ok(BinOp::Add),
+        '-' => Ok(BinOp::Sub),
+        '*' => Ok(BinOp::Mul),
+        '/' => Ok(BinOp::Div),
+        '%' => Ok(BinOp::Mod),
+        '<' => Ok(BinOp::Lt),
+        '>' => Ok(BinOp::Gt),
+        '=' => Ok(BinOp::Eq),
+        '|' => Ok(BinOp::Or),
+        '&' => Ok(BinOp::And),
+        '.' => Ok(BinOp::Concat),
+        'T' => Ok(BinOp::Take),
+        'D' => Ok(BinOp::Drop),
+        '$' => Ok(BinOp::Apply),
+        _ => Err(ParseError::InvalidBinaryOp(body.into())),
+    }
 }
 
 fn parse(tokens: &[Token]) -> ParseResult<(Box<Node>, &[Token])> {
@@ -179,11 +230,42 @@ fn parse(tokens: &[Token]) -> ParseResult<(Box<Node>, &[Token])> {
         'F' => ok(Node::Literal(Value::Bool(false)), &tokens[1..]),
         'I' => parse_integer(body).map(|node| (node, &tokens[1..])),
         'S' => parse_string(body).map(|node| (node, &tokens[1..])),
+        'U' => {
+            let op = parse_unary_op(body)?;
+            let (operand, rest) = parse(&tokens[1..])?;
+            ok(Node::UnaryOp(op, operand), rest)
+        }
+        'B' => {
+            let op = parse_bin_op(body)?;
+            let (lhs, rest1) = parse(&tokens[1..])?;
+            let (rhs, rest2) = parse(rest1)?;
+            ok(Node::BinOp(op, lhs, rhs), rest2)
+        }
+        '?' => {
+            let (cond, rest1) = parse(&tokens[1..])?;
+            let (then, rest2) = parse(rest1)?;
+            let (else_, rest3) = parse(rest2)?;
+            ok(Node::If(cond, then, else_), rest3)
+        }
+        'L' => {
+            let var = parse_integer_raw(body)? as usize;
+            let (expr, rest) = parse(&tokens[1..])?;
+            ok(Node::Lambda(var, expr), rest)
+        }
+        'v' => {
+            let var = parse_integer_raw(body)? as usize;
+            ok(Node::Variable(var), &tokens[1..])
+        }
         _ => Err(ParseError::UnknownIndicator(indicator)),
     }
 }
 
-pub fn eval(input: &str) -> Value {
+pub fn eval(input: &str) -> ParseResult<Value> {
     let tokens = tokenize(input);
+    let (ast, rest) = parse(&tokens)?;
+    if !rest.is_empty() {
+        return Err(ParseError::UnexpectedEof);
+    }
+    println!("AST: {ast}");
     todo!()
 }

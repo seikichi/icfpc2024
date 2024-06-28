9c540f960a8632b9dc5be5a14b42bf9a3592a012
Yusuke Nojima <nojima@ynojima.com>
2024-06-28 21:28:57 +0900

    Add decode-string

---
+++ scripts/decode-string.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+input = $stdin.read.strip
+if input[0] != "S" then
+    raise RuntimeError("not a string")
+end
+
+TABLE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`|~ \n"
+
+ret = ""
+for c in input[1..].chars
+    ret += TABLE[c.ord - 33]
+end
+puts ret

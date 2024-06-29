e9aa9ec5f00d78f69f878166a817306b8909063e
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-06-29 13:11:52 +0900

    Fix

--- lambda/lambda.ts
+++ lambda/lambda.ts
@@ -22,7 +22,53 @@ export const solver: Handler = async (rawEvent, _context) => {
     ...event,
     commitId: env.COMMIT_ID,
     tmpDir: "/tmp",
-    courseDir: "./cources",
+    courseDir: "./courses",
     solverDir: path.join("target", "release"),
   });
 };
+
+// const ExperimentEvent = z.object({
+//   course: z.string(),
+//   levels: z.string().min(1).max(32),
+//   tag: z.string().max(16),
+//   args: z.string().max(256),
+// });
+
+// export const experiment: Handler = async (rawEvent, _context) => {
+//   // const env = Env.parse(process.env);
+//   const event = ExperimentEvent.parse(rawEvent);
+
+//   // TODO: insert record
+
+//   // Parse levels (e.g., "1,2,3-5,7" => {1, 2, 3, 4, 5, 7})
+//   const ids: Set<number> = new Set([]);
+//   for (const ps of event.levels.split(",")) {
+//     if (ps.includes("-")) {
+//       const [fromS, toS] = ps.split("-");
+//       const [from, to] = [parseInt(fromS, 10), parseInt(toS, 10)];
+//       for (let i = from; i <= to; i++) {
+//         ids.add(i);
+//       }
+//     } else {
+//       ids.add(parseInt(ps, 10));
+//     }
+//   }
+
+//   for (const level of Array.from(ids)) {
+//       console.log(`invoke: ${event.course}${level}`);
+
+//     const solverEvent: z.infer<typeof SolverEvent> = {
+//       course: event.course,
+//       level,
+//       args: event.args,
+//     };
+
+//     await lambda.send(
+//       new InvokeCommand({
+//         InvocationType: InvocationType.Event,
+//         FunctionName: env.SOLVER_LAMBDA_ARN,
+//         Payload: JSON.stringify(solverEvent),
+//       })
+//     );
+//   }
+// };

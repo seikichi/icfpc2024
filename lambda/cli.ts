0b866eb3a4908247358146382992c186782e9e5c
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-06-29 12:27:55 +0900

    Update lambda

---
+++ lambda/cli.ts
@@ -0,0 +1,16 @@
+import { solve } from "./solver";
+import * as path from "path";
+
+// テスト用
+
+(async () => {
+  await solve({
+    course: "lambdaman",
+    level: 1,
+    courseDir: path.join("..", "courses"),
+    solverDir: path.join("..", "solver", "target", "debug"),
+    tmpDir: "./tmp",
+    args: "--ai Simple,Simple",
+    commitId: "DUMMY",
+  });
+})();

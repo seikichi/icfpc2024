07e0cea3d5c60d116234f41a7ad6861f6db0e611
Yusuke Nojima <nojima@ynojima.com>
2024-07-01 02:02:37 +0900

    improve

--- courses/lambdaman/solution/lambdaman16.hs
+++ courses/lambdaman/solution/lambdaman16.hs
@@ -4,14 +4,14 @@
         if order == 0 then
             ""
         else
-            let next = order - 1 in (
-              (self self next c d a b) .
-              d .
-              (self self next a b c d) .
-              b .
-              (self self next a b c d) .
-              c .
-              (self self next d c b a)
+            let next = self self (order - 1) in (
+                (next c d a b) .
+                d .
+                (next a b c d) .
+                b .
+                (next a b c d) .
+                c .
+                (next d c b a)
             )
     )
 ) 6 "LLLL" "RRRR" "UUUU" "DDDD"

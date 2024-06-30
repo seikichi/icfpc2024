99af4fdb8979eca19ab9bcb7d0014c5d30b3db18
Yusuke Nojima <nojima@ynojima.com>
2024-06-30 11:38:01 +0900

    fix indent

--- scripts/unpackint.hs
+++ scripts/unpackint.hs
@@ -1,14 +1,14 @@
 -- packint.rb でパックした整数をもとの文字列に戻す
-1 D ((
-  (\x ->
-    (\y -> x (y y)) (\y -> x (y y))
-  ) (\self -> \code ->
-    if code == 0 then
-      ""
-    else
-      (self (code / 4)) . (1 T ((code % 4) D "LRDU"))
+1 D (
+  (
+    (\x ->
+      (\y -> x (y y)) (\y -> x (y y))
+    ) (\self -> \code ->
+      if code == 0 then
+        ""
+      else
+        (self (code / 4)) . (1 T ((code % 4) D "LRDU"))
+    )
   )
+  1085634933 -- ←この数字を問題の解答を packint したものに書き換える
 )
-1085634933 -- ←この数字を問題の解答を packint したものに書き換える
-)
-

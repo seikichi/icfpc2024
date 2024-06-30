d1b37c82344e2812cf69bdc898133d2435df6db6
Yusuke Nojima <nojima@ynojima.com>
2024-06-30 16:44:37 +0900

    improve lambdaman6

--- courses/lambdaman/solution/lambdaman6.hs
+++ courses/lambdaman/solution/lambdaman6.hs
@@ -1,11 +1,8 @@
 -- R を 199 回出力
--- Yコンビネータ
--- Y = λf . (λx . f (x x)) (λx . f (x x))
-let Y = \f -> (\x -> f (x x)) (\x -> f (x x)) in
-
 "solve lambdaman6 " . (
   (
-    Y (\self -> \n ->
+    (\f -> (\x -> f (x x)) (\x -> f (x x))) -- Yコンビネータ
+    (\self -> \n ->
       if n == 0 then
         ""
       else

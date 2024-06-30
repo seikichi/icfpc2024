b08fd53597ab24890c682d764aaa02b98c76e8a3
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-07-01 01:55:37 +0900

    Update 16

--- courses/lambdaman/solution/lambdaman16.hs
+++ courses/lambdaman/solution/lambdaman16.hs
@@ -1,17 +1,17 @@
-let repeat5 = \s -> s . s . s . s . s in
-
 "solve lambdaman16 " .
 (\f -> f f) (
     \self -> \order -> \a -> \b -> \c -> \d -> (
         if order == 0 then
             ""
         else
-            (self self (order - 1) c d a b) .
-            (repeat5 d) .
-            (self self (order - 1) a b c d) .
-            (repeat5 b) .
-            (self self (order - 1) a b c d) .
-            (repeat5 c) .
-            (self self (order - 1) d c b a)
+            let next = order - 1 in (
+              (self self next c d a b) .
+              d .
+              (self self next a b c d) .
+              b .
+              (self self next a b c d) .
+              c .
+              (self self next d c b a)
+            )
     )
-) 6 "L" "R" "U" "D"
+) 6 "LLLL" "RRRR" "UUUU" "DDDD"

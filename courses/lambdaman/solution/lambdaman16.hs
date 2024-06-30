05b3bb94fa62baf7ea7626b6077c27d5019827ac
Yusuke Nojima <nojima@ynojima.com>
2024-07-01 01:48:58 +0900

    shorten lambdaman16

--- courses/lambdaman/solution/lambdaman16.hs
+++ courses/lambdaman/solution/lambdaman16.hs
@@ -1,18 +1,18 @@
 let Y = \f -> (\x -> f (x x)) (\x -> f (x x)) in
-let repeat = \s -> Y (\self -> \n -> if n == 0 then "" else (self (n - 1)) . s) in
+let repeat5 = \s -> s . s . s . s . s in
 
-let hilbert = Y
-(
+"solve lambdaman16 " .
+Y (
     \self -> \order -> \a -> \b -> \c -> \d -> (
         if order == 0 then
             ""
         else
             (self (order - 1) c d a b) .
-            (repeat d 5) .
+            (repeat5 d) .
             (self (order - 1) a b c d) .
-            (repeat b 5) .
+            (repeat5 b) .
             (self (order - 1) a b c d) .
-            (repeat c 5) .
+            (repeat5 c) .
             (self (order - 1) d c b a)
     )
-) in "solve lambdaman16 " . (hilbert 6 "L" "R" "U" "D")
+) 6 "L" "R" "U" "D"

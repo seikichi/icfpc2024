e63a58c874c0b4838f5f040990dc3cd668852d40
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-07-01 19:16:28 +0900

    Update 21

--- courses/lambdaman/solution/lambdaman21.hs
+++ courses/lambdaman/solution/lambdaman21.hs
@@ -4,28 +4,33 @@
   let rR = repeat "R" in
   let rD = repeat "D" in
   let rL = repeat "L" in
+  let rU200 = rU 200 in
+  -- let rD200 = rD 200 in
+  let rR200 = rR 200 in
+  let rL200 = rL 200 in
+  let rL10 = rL 10 in
   (
-    -- 一旦左上
-    (rU 71) . (rL 76) .
-    -- 塗れるだけ塗る
-    (repeat ((rR 199) . "D" . (rL 199)) 199) .
+    -- 一旦左上 (U, L 適当)
+    rU200 . rL200 .
+    -- 塗れるだけ塗る (199 より大きくても良い)
+    (repeat (rR200 . "D" . rL200) 199) .
     -- まずは 3 の左の下に
     (rU 50) .
-    -- 内側になるまで塗る
-    (repeat ((rR 100) . (rL 100) . "U") 105) .
+    -- 内側になるまで塗る (rR と RL 適当でいい)
+    (repeat (rR200 . rL200 . "U") 105) .
     -- 3の塗り残し (上)
     (rD 17) .
     (rR 18) .
-    "U" . (repeat ((rR 40) . (rL 10) . "U" . (rL 30)) 5) .
+    "U" . (repeat (rR200 . rL10 . "U" . rL200) 5) .
     -- 3の塗り残し (下)
     (rD 10) . (rL 21) . (rD 66) .
-    (repeat ((rR 50) . (rL 10) . "D" . (rL 40)) 7) .
+    (repeat (rR200 . rL10 . "D" . rL200) 7) .
     -- 3D の間
     (rU 7) . (rL 22) . (rD 19) . (rR 91) . (rU 3) .
-    (repeat ((rL 50) . (rR 50) . "U") 101) .
+    (repeat (rL200 . rR200 . "U") 101) .
     -- D にいく
     (rR 86) . (rD 49) . (rL 15) . "U" .
     -- D ぬる
-    (repeat ((rD 80) . "L" . (rU 80)) 56)
+    (repeat ((rD 80) . "L" . rU200) 56)
   )
 )

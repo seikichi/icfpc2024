f68dfe296fa465f71542df046171deebdd347a46
Seikichi KONDO <seikichi@kmc.gr.jp>
2024-07-01 16:41:35 +0900

    Update 10

--- courses/lambdaman/solution/lambdaman10.hs
+++ courses/lambdaman/solution/lambdaman10.hs
@@ -1,8 +1,10 @@
 "solve lambdaman10 " . (
   let f = \s -> s . s . s . s . s in
   let r = \s -> f (f s) in
+  let rrR_DRRU = r (r "R" . "DRRU") in
+  let rrL_ULLD = r (r "L" . "ULLD") in
   f (
-    r (r (r "R" . "DRRU") . "LDR" . r (r "L" . "ULLD") . "RDL")
-    . (r (r "R" . "DRRU") . "DLDR" . r (r "L" . "ULLD") . "RDLU")
+    r (rrR_DRRU . "LDR" . rrL_ULLD . "RDL")
+    . (rrR_DRRU . "DLDR" . rrL_ULLD . "RDLU")
   )
 )
